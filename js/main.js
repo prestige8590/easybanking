2454245190eaa96365a7b3b6d1cd8e74d6281f9a
prestige8590 <victorykelvin946@gmail.com>
2024-04-11 14:37:21 +0100

    update document

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const menuOpen = document.querySelector('.ri-menu-fill')
+const menuClose = document.querySelector('.ri-close-fill')
+const menu = document.querySelector('nav ul')
+
+menuOpen.addEventListener('click', function(){
+    menu.style.top = '45%'
+    menuClose.style.display = 'block'
+    menuOpen.style.display = 'none'
+})
+
+menuClose.addEventListener('click', function(){
+    menu.style.top = '-40%'
+    menuClose.style.display = 'none'
+    menuOpen.style.display = 'block'
+})
+
+document.querySelectorAll('nav ul li').forEach(link => {
+    link.addEventListener('click', function(){
+        menu.style.top = '-40%'
+        menuClose.style.display = 'none'
+        menuOpen.style.display = 'block'
+    })
+})
+
+document.querySelector('.ri-arrow-up-s-line').addEventListener('click', function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+window.addEventListener("scroll", function() {
+    //when the user scrolls 30px from the top, show the 'back-to-top' button(first for crome, second for Safari)
+    if(document.documentElement.scrollTop >= 30 || document.body.scrollTop >= 30) {
+        document.querySelector('.ri-arrow-up-s-line').style.display = "block"
+    } else{
+        document.querySelector('.ri-arrow-up-s-line').style.display = "none"
+    }
+})
